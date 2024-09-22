4e6c7beb4ab3757ade5f7ccd076c5fb6b1edb3ef
emiliya.chavdarova <emiliya.chavdarova@colleto.de>
2024-09-22 18:48:52 +0200

    Add routers and controllers

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
 
+import routes from './routes.js';
+
 
 const app = express();
 
@@ -8,14 +10,14 @@ app.engine('hbs', handlebars.engine({
     extname: 'hbs'
 }));
 
+// sets view engine and extentions
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
+// sets up static files dir
 app.use(express.static('public'));
 
-
-app.get('/', (req, res) => {
-    res.render('home');
-});
+// routes as middleware
+app.use(routes);
 
 app.listen(5100, () => console.log('Server is listening on http://localhost:5100'));
